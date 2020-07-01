4d6470f8cb4eca7783ab095f7e1827a3d8d2bb63
Allan Reyes <chef@ulampinoy.com>
2020-07-01 11:39:41 +0200

    Add a test entry to the sidebar

--- sidebars.js
+++ sidebars.js
@@ -4,7 +4,7 @@ module.exports = {
       type: 'category',
       label: 'Bookingpad',
       collapsed: false,
-      items: ['overview', 'shopping', 'booking', 'order-management', 'create-booking-onhold', 'issue-booking-onhold', 'glossary'],
+      items: ['overview', 'shopping', 'booking', 'order-management', 'create-booking-onhold', 'issue-booking-onhold', 'glossary', 'test-for-fede'],
     },
     {
       type: 'category',
